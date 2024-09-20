d7b64ea8cc6f7a641ba983759917e56ba5700759
catarina p <catarinampestana@ua.pt>
2024-09-20 11:41:17 +0100

    Creating a class on a module, that I want to export

--- modules.js
+++ modules.js
@@ -1,2 +1,7 @@
 console.log(arguments); //Arguments -  array in js, that contains all the values that are passed to the function (export, require funtion, module, __filename, __dirname) > 
 console.log(require("module").wrapper); // returns the wrapper function, it's the "template" node uses to create the wrapper function for every kodule and then fills it with our code
+
+const C = require('./test-module-1'); //Use upper case for classes
+
+const calc1 = new C();
+console.log(calc1.add(2,5));
