ffda88fd26539dc928bcefa5de825262192f8c80
catarina p <catarinampestana@ua.pt>
2024-09-20 11:46:12 +0100

    how to export stuff with module.exports, assigning the value we want to module exports

--- test-module-1.js
+++ test-module-1.js
@@ -1,4 +1,4 @@
-class Calculator {
+module.exports = class {  // to immediatly assign the value that we want the odule to exports
 
     add(a,b) {
         return a + b;
@@ -13,4 +13,22 @@ class Calculator {
     }
 }
 
-module.exports = Calculator; // use module.exports when you want to export one single value (eg one class, one function)
+
+
+
+// class Calculator {
+
+//     add(a,b) {
+//         return a + b;
+//     }
+
+//     multiply(a, b) {
+//         return a * b;
+//     }
+
+//     divide(a, b){
+//         return a / b;
+//     }
+// }
+
+// module.exports = Calculator; // use module.exports when you want to export one single value (eg one class, one function)
